b1790bb5ba2bf8bcf9a6fc2d8a936a121255549e
Mohan <mohan.nimmala@bluehublabs.net>
2023-06-25 19:59:44 +0530

    Navigation Bar

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavigatioBar from '../Components/NavigatioBar';
+
+export const LandingPage = () => {
+  return (
+    <div>
+        <NavigatioBar />
+    </div>
+  )
+}
